0593ccc4fb9b3e697eedb46cbc8a45fb9e18dd44
Widukind42 <Widukind42@proton.me>
2026-09-01 18:08:45 +0200

    Show versions of yt-dlp-ejs and curl_cffi
    Added commands to display versions of yt-dlp-ejs and curl_cffi.

--- .github/scripts/show-container-releases.sh
+++ .github/scripts/show-container-releases.sh
@@ -4,6 +4,8 @@
 echo "- $PRETTY_NAME"
 echo "- $(python3 --version)"
 echo "- yt-dlp version $(yt-dlp --version)"
+echo "  - yt-dlp-ejs version $(python -c "from importlib.metadata import version; print(version('yt-dlp-ejs'))")"
+echo "  - curl_cffi version $(python -c "from importlib.metadata import version; print(version('curl_cffi'))")"
 echo "- $(ffmpeg -version | head -n 1)"
 echo "- $(ffprobe -version | head -n 1)"
 echo "- $(deno --version | head -n 1)"
